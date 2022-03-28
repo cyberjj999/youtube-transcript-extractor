6c1446be0f583d58e36e504ad2e5689a7887353e
cyberjj999 <lyejiajun99@gmail.com>
2022-03-28 21:12:04 +0800

    Add new python script for youtube transcript extraction for new YouTube HTML DOM

--- get-youtube-transcript.js
+++ get-youtube-transcript.js
@@ -22,6 +22,27 @@ function formatTranscriptText(text) {
 // Function to check if transcript is available
 function isTranscriptAvailable() {
     transcriptAvailable = true;
+    
+    // const wait=ms=>new Promise(resolve => setTimeout(resolve, ms));
+    
+    // // Click more actions (open dropdown to populate option for opening transcript)
+    // let menu = document.getElementById("menu-container").getElementsByTagName("yt-icon-button");
+    // for (var i = 0; i < menu.length; i++) {
+    //     if (menu[i].className == "dropdown-trigger style-scope ytd-menu-renderer") {
+    //         menu[i].click();
+    //     }
+    // }
+
+    // // Open Transcript
+    // setTimeout(function () {
+    //     // Use timeout to prevent null error
+    //     // var x = document.querySelector('#items > ytd-menu-service-item-renderer > tp-yt-paper-item');
+    //     var x = document.querySelector("#items > ytd-menu-service-item-renderer:nth-child(2) > tp-yt-paper-item");
+    //     x.click();
+    // }, 500);
+    
+    
+    /* This code does not work when there is advertisement with subtitles - please refer to the updated code above */
     let captionButton = document.querySelector(
         "#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-right-controls > button.ytp-subtitles-button.ytp-button"
     );
@@ -37,12 +58,52 @@ function isTranscriptAvailable() {
     return transcriptAvailable;
 }
 
+function copyTextToClipboard(transcriptText){    
+    // Quick Formatting (feel free to remove)
+    paragraphText = formatTranscriptText(transcriptText);        
+    navigator.clipboard
+        .writeText(paragraphText)
+        .then(
+            () => {
+                //clipboard successfully set
+                alert("Copy Successful!");
+            },
+            () => {
+                //clipboard write failed, use fallback
+                alert("Copy Failed!");
+            }
+        )
+        .catch((err) => {
+            console.error("Failed to read clipboard contents: ", err);
+        });        
+}
+
 function extractTranscriptText() {
     transcriptAvailable = isTranscriptAvailable();
     if (transcriptAvailable == false ) return
 
-    alert("Starting Transcript Extraction...");
+    alert("Starting Transcript Extraction...");    
     var transcriptText = "";
+
+    let transcriptBody = document.querySelector("#body > ytd-transcript-body-renderer");
+    if (transcriptBody != null) {        
+        let transcriptBodyList = document.querySelector("#body > ytd-transcript-body-renderer").childNodes;
+        // Loop through all transcriptBodyList and extract transcript text
+        for (var i = 0; i < transcriptBodyList.length; i++) {
+            var transcriptChild = transcriptBodyList[i]; // extract i-th child
+            if (transcriptChild != undefined) { // check if child is defined
+                var transcriptChildText = document.querySelectorAll("div.cues.style-scope.ytd-transcript-body-renderer > div")[i];
+                if (transcriptChildText != undefined) {
+                    transcriptChildText = transcriptChildText.innerText;
+                    transcriptText += transcriptChildText + " ";
+                }
+                // alert(transcriptChildText);
+            }
+        }
+        copyTextToClipboard(transcriptText);
+        return
+    }
+    
     // Click more actions (open dropdown to populate option for opening transcript)
     let menu = document.getElementById("menu-container").getElementsByTagName("yt-icon-button");
     for (var i = 0; i < menu.length; i++) {
@@ -54,7 +115,6 @@ function extractTranscriptText() {
     // Open Transcript
     setTimeout(function () {
         // Use timeout to prevent null error
-        // var x = document.querySelector('#items > ytd-menu-service-item-renderer > tp-yt-paper-item');
         var x = document.querySelector("#items > ytd-menu-service-item-renderer:nth-child(2) > tp-yt-paper-item");
         x.click();
     }, 500);
@@ -65,7 +125,7 @@ function extractTranscriptText() {
         // Select Transcript Body's ChildNodes List (i.e. all child tags that contain transcript text + timestamp)
         let transcriptBody = document.querySelector("#body > ytd-transcript-body-renderer");
         if (transcriptBody == null) {
-            alert("transcriptBody is null!!");
+            alert("Error! Transcript Body Cannot Be Found!");
         }
         let transcriptBodyList = document.querySelector("#body > ytd-transcript-body-renderer").childNodes;
         // Loop through all transcriptBodyList and extract transcript text
@@ -80,26 +140,9 @@ function extractTranscriptText() {
                 // alert(transcriptChildText);
             }
         }
-        // Quick Formatting (feel free to remove)
-        paragraphText = formatTranscriptText(transcriptText);        
-        navigator.clipboard
-            .writeText(paragraphText)
-            .then(
-                () => {
-                    //clipboard successfully set
-                    alert("Copy Successful!");
-                },
-                () => {
-                    //clipboard write failed, use fallback
-                    alert("Copy Failed!");
-                }
-            )
-            .catch((err) => {
-                console.error("Failed to read clipboard contents: ", err);
-            });
+        copyTextToClipboard(transcriptText);
         // // alert(transcriptText);
         // return transcriptText;'
-        return "test";
     }, 3000);
 }
 
---
+++ youtube_transcript_extractor.py
@@ -0,0 +1,35 @@
+# https://pypi.org/project/youtube-transcript-api/
+# pip install youtube_transcript_api
+from youtube_transcript_api import YouTubeTranscriptApi
+
+# input video link
+video_url = 'https://www.youtube.com/watch?v=rkBk2BCFFxc'
+video_id = video_url.split('?v=')[1]                            # extract video id from link
+print('Video URL:', video_url, '| Video ID Extracted:', video_id)
+print('Extracting Transcript From', video_url)
+transcript_list = YouTubeTranscriptApi.get_transcript(video_id) # get youtube transcript
+
+print('Extracting Transcript Text...')
+all_transcript_text = ''
+for each_transcript_segment in transcript_list:                 # traverse through each transcript segment
+    transcript_text = each_transcript_segment['text']           # extract transcript text for each segment
+    transcript_text = transcript_text.replace(u'\xa0', u'')     # remove \xa0 (which is a non-breaking space in Latin1 (ISO 8859-1))
+    all_transcript_text += transcript_text + ' '
+
+print('Formatting Transcript Text...')
+# optional - format the transcript in chunks so it is more readable
+all_transcript_text_in_words_list = all_transcript_text.split()
+word_counter = 0
+transcript_paragraph = ''
+for each_word in all_transcript_text_in_words_list:
+    if word_counter == 60: 
+        transcript_paragraph+= '\n\n'
+        word_counter = 0
+    transcript_paragraph += each_word + ' '
+    word_counter += 1
+
+print('Writing Transcript Text to File...')
+with open('transcript.txt', 'w', encoding = 'utf-8') as f:
+    f.write(transcript_paragraph)
+
+print('Transcript Extraction Successful! Transcript Extracted to transcript.txt')
